ef32fb4b20e7644de3cf4bb18660f086c6bbb345
daeho <daeho210@gmail.com>
2024-12-18 17:41:06 +0900

    Refactor: 단위테스트 한글화 빠진 부분 한글화

--- 1.aboutTDD/src/point/point.service.spec.ts
+++ 1.aboutTDD/src/point/point.service.spec.ts
@@ -42,8 +42,7 @@ describe('PointService', () => {
 
     describe('포인트 조회 단위 테스트', () => {
         describe('성공 케이스', () => {
-            // 1. 유효한 사용자 ID가 제공되면 올바른 사용자 포인트 정보를 반환합니다.
-            it('should return the correct user point information when a valid user ID is provided', async () => {
+            it('유효한 사용자 ID가 제공되면 올바른 사용자 포인트 정보를 반환합니다.', async () => {
                 const userId = 1;
                 const now = Date.now();
 
@@ -57,18 +56,20 @@ describe('PointService', () => {
             });
         });
         describe('실패 케이스', () => {
-            // 1. 유효하지 않은 사용자 ID가 제공되면 예외를 발생시킵니다.
             it.each([
                 { userId: null, description: 'null' },
                 { userId: 0, description: '0' },
                 { userId: -1, description: 'negative number' },
                 { userId: NaN, description: 'NaN' },
                 { userId: undefined, description: 'undefined' },
-            ])('should throw an error when userId is $description', async ({ userId }) => {
-                await expect(service.getPoint(userId)).rejects.toThrow(
-                    '올바르지 않은 ID 값 입니다.',
-                );
-            });
+            ])(
+                '유효하지 않은 사용자 ID가 제공되면 예외를 발생시킵니다. userId is $description',
+                async ({ userId }) => {
+                    await expect(service.getPoint(userId)).rejects.toThrow(
+                        '올바르지 않은 ID 값 입니다.',
+                    );
+                },
+            );
         });
     });
 
